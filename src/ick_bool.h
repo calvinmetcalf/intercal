eae8a3ac3126dc9364bb9625424554bd6c6e4b7c
Alex Smith <ais523@bham.ac.uk>
2010-09-02 19:39:31 +0100

    Boolean fixes + autoconfiscation of booleans
    Cleaning up ick_bool.h to work even on systems with an insane
    stdbool.h; fixing oil.y to use the new booleans throughout;
    integrating ick_bool.h into the autoconf system.

--- src/ick_bool.h
+++ src/ick_bool.h
@@ -1,19 +1,26 @@
 /*
  * Hide difference between compilers with the C99 bool type and those without.
  * The main reason this is desirable is so splint can do more rigorous checking
- * on bools.
+ * on bools. This also allows for compilers that define _Bool but not true or
+ * false, etc.
  */
 
 /*@-redef@ -incondefs*/
-#ifdef __STDC_VERSION__
-#if __STDC_VERSION__ >= 199901L
-#include <stdbool.h>
-#endif
-#endif
-#ifndef __bool_true_false_are_defined
+#ifdef HAVE_STDBOOL_H
+# include <stdbool.h>
+#else
+# ifndef HAVE__BOOL
+#  ifdef HAVE_STDINT_H
+#   include <stdint.h>
+typedef int_fast8_t bool;
+#  else
 typedef int bool;
-#define true 1
-#define false 0
-#define __bool_true_false_are_defined 1
+#  endif
+# else
+typedef _Bool bool
+# endif
+# define true 1
+# define false 0
+# define __bool_true_false_are_defined 1
 #endif
 /*@=redef =incondefs@*/
